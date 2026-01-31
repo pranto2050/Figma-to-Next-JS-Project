d83f96baadf9982bfe4e124b5ac78ea9722e8c28
pranto-islam <prantoislamnt51@gmail.com>
2026-01-31 17:44:30 +0600

    Max Width Solve

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,7 +37,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body suppressHydrationWarning>
-        <div className="max-w-[1253px] mx-auto w-full min-h-screen">
+        <div className="max-w-[1440px] mx-auto w-full min-h-screen">
           {children}
         </div>
       </body>
